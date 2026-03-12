4fa85bd815b24c46a16c209cd92ec5f55f1fbdf3
raj <webman.raj@gmail.com>
2026-03-12 20:35:26 +0530

    yes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const API = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL:API,
   withCredentials: true, // Important for cookies
 });
 
